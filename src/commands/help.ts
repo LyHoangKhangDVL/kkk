860095d93ba6aa131d8c11174fe9dbb2c657b569
LyHoangKhangDVL <121933718+LyHoangKhangDVL@users.noreply.github.com>
2024-02-23 22:39:46 +0700

    Update help.ts

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -7,7 +7,7 @@ export default {
         let document = ""
         const listCommand = Object.keys(global.commands)
         for(const command of listCommand) document += `**${command}:** ${global.commands[command].info}\n`
-        document += "Join Our Support Server For Help: https://discord.gg/Yr92g5Zx3e"
+        document += ""
         if(args[0]) message.reply(
             listCommand.includes(args[0]) 
             ? `**${args[0]}:** ${global.commands[args[0]].info}`
